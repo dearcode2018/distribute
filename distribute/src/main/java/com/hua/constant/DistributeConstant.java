e390aeee9aa08786d512e0651399af108ec63ba4
qianye.zheng <dearzqy@163.com>
2018-04-12 13:21:38 +0800

    no message

---
+++ distribute/src/main/java/com/hua/constant/DistributeConstant.java
@@ -0,0 +1,16 @@
+/**
+ * DistributeConstant.java
+ * @author  qye.zheng
+ * 	version 1.0
+ */
+package com.hua.constant;
+
+/**
+ * DistributeConstant
+ * 描述: 分布式 - 常量 
+ * @author  qye.zheng
+ */
+public interface DistributeConstant
+{
+
+}
